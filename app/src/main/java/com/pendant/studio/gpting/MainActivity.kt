d10741efcbbfe92f33a6dd291b4256d52ee9cbd5
Donghan Kim <54224868+pendant-k@users.noreply.github.com>
2023-05-23 15:46:22 +0900

    first commit

---
+++ app/src/main/java/com/pendant/studio/gpting/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.pendant.studio.gpting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // bottom Navigator
+
+        loadFragment(SearchFragment())
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        // Add bottom Navigation event
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.bottom_search_item-> {
+                    loadFragment(SearchFragment())
+                    true
+                }
+                R.id.bottom_storage_item-> {
+                    loadFragment(StorageFragment())
+                    true
+                }
+                R.id.bottom_setting_item-> {
+                    loadFragment(SettingFragment())
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+
+}
